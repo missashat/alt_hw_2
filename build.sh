f092305f1f466c178e43dabbb6c793472e8e91e6
Asha Turman <keepin.it.classy@gmail.com>
2018-06-03 17:11:12 -0700

    created templates, docs, content

---
+++ build.sh
@@ -0,0 +1,4 @@
+cat templates/index_top.html content/index.html templates/index_bottom.html > docs/index.html
+cat templates/conc_att_top.html content/conc_att.html templates/conc_att_bottom.html > docs/conc_att.html
+cat templates/conc_photos_top.html content/conc_photos.html templates/conc_photos_bottom.html > docs/conc_photos.html
+cat templates/conc_vids_top.html content/conc_vids.html templates/conc_vids_bottom.html > docs/conc_vids.html
